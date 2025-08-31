1cb8664db7aaa40111173217705c83896253ace4
Kenish <knsh3589@gmail.com>
2025-08-31 16:00:50 +0545

    learnt about opetaion.js properly

---
+++ operation.js
@@ -0,0 +1,21 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log(typeof negvalue);
+
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1 + " " + str2
+console.log(str3);
+
+console.log("5" + 1); // "51"
+console.log("5" - 1); // 4
+console.log("5" * 2); // 10
+console.log(1+1)    // 2
+console.log(1-1)    // 0
+console.log(1*1)    // 1
+console.log(1/1)    // 1
+console.log(5%2)    // 1
+console.log(5**2)   // 25
+console.log("1"+2+2); // "122"
+console.log(1+2+"2"); // "32"
