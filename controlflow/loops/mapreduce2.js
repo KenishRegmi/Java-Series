318478dade96672f003c65c60fa0b63ab17dc603
Kenish <knsh3589@gmail.com>
2025-09-08 13:17:08 +0545

    map and reduce learnt completely

--- controlflow/loops/mapreduce2.js
+++ controlflow/loops/mapreduce2.js
@@ -1,4 +1,10 @@
 const myNumbers = [1,2,3,4,5,6,7,8,9,10];
 
-let newNums = myNumbers.map((num) => num + 10)
+let newNums = myNumbers.map((num) => num + 10) // map returns a new array after performing the operation
 console.log(newNums);
+
+
+let newNums2 = myNumbers.map((num) => { // if you use chaining then the num value passes to the another function
+    return num + 10
+}).map((num) => num * 2)
+console.log(newNums2);
---
+++ controlflow/loops/mapreduce3.js
@@ -0,0 +1,39 @@
+//--------------------------------Reduce Method--------------------------------
+//accumulator is like a container that holds the value
+//current value is the current element being processed in the array
+
+const nums = [1,2,3,4,5];
+const sum = nums.reduce(function (acc, curval) {
+    console.log(acc, curval);
+    
+     return acc + curval
+    },0) // 0 is the initial value of accumulator
+
+console.log(sum);
+
+
+const multiply = nums.reduce((acc, curval) => acc * curval, 1) // 1 is the initial value of accumulator
+console.log(multiply);
+
+//real life application of reduce method
+const cart = [
+    {
+        name: "laptop",
+        price: 30000,
+    },
+    {
+        name: "phone",
+        price: 20000,
+    },
+    {
+        name: "tablet",
+        price: 15000,
+    },
+    {
+        name: "monitor",
+        price: 25000,
+    }
+]
+
+const totalPrice = cart.reduce((acc, item) => acc + item.price, 0)
+console.log(totalPrice);
