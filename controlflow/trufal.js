d2e8e0e5debb910224b803ed470074d5e7568609
Kenish <knsh3589@gmail.com>
2025-09-08 10:35:39 +0545

    loops

--- controlflow/trufal.js
+++ controlflow/trufal.js
@@ -16,3 +16,19 @@ console.log(userMail)
 // Truthy and Falsy values
 // Falsy Values: false, 0,,-0, Bigint 0n "", null, undefined, NaN
 // Truthy Values: "0" , "false" in string, [], {}, function(){}
+
+const obj = {}
+
+if (Object.keys(obj).length === 0){
+    console.log("Object is empty")
+}
+
+let val1 
+val1 = null ?? 101 //
+console.log(val1)
+
+//ternary operator
+//Syntax: condition ? expr1 : expr2
+const number = 10
+const result = number % 2 === 0 ? "Even" : "Odd"
+console.log(result)
