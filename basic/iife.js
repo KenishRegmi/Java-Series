209514661e66792d0791660392727702100479e4
Kenish <knsh3589@gmail.com>
2025-09-08 10:06:13 +0545

    learning control flow

---
+++ basic/iife.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expression (IIFE)
+// Syntax: (function() { // function body })();
+(function() {
+    console.log("This is an IIFE");
+})(); // IIFE is executed immediately after its definition
+
+// Global scope pollution is avoided because variables and functions defined inside IIFE are not accessible outside
+
---
+++ control flow/controlflow.js
@@ -0,0 +1,5 @@
+//----------------------------------Control Flow Statements----------------------------------//
+// Control flow statements are used to control the flow of execution in a program based on certain conditions or loops.
+// They allow you to make decisions, repeat actions, and manage the flow of your code effectively.
+// Here are some common control flow statements in JavaScript:
+
