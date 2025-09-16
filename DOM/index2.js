634712db8d48aeffca037173252eb08e0639169f
Kenish <knsh3589@gmail.com>
2025-09-16 11:21:44 +0545

    basicsofjs

--- DOM/index2.js
+++ DOM/index2.js
@@ -1,5 +1,17 @@
 const parent = document.querySelector('.week');
-const child = document.querySelector('.day');
-console.log(parent);
-console.log(child);
+const children = parent.children; // HTMLCollection of .day divs
 
+// for (let i = 0; i < children.length; i++) {
+//     console.log(children[i].textContent);
+// }
+// children[1].style.color = 'orange'
+console.log(children.firstElementChild);
+
+const day1 = document.querySelector('.day')
+console.log(day1);
+
+console.log(day1.parentElement); // gives the parent value
+console.log(day1.nextElementSibling); // gives the next element 
+
+console.log("NODES: ",parent.childNodes);
+//9 
